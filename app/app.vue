2b080fdc40e1e10a51ca035c2d63d4cf30b9e8fe
Tony Duong <tony.duong.102@gmail.com>
2026-02-04 23:38:53 +0900

    Add @vercel/analytics integration for enhanced analytics tracking in the application

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </NuxtLayout>
+  <Analytics />
 </template>
